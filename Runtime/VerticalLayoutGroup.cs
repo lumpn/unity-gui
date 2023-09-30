fcecef55b9986200c7177bb30e3f1fb1a4a68010
lumpn <lumpn@users.noreply.github.com>
2023-09-30 17:33:22 +0800

    Implementing vertical layout

--- Runtime/VerticalLayoutGroup.cs
+++ Runtime/VerticalLayoutGroup.cs
@@ -15,7 +15,22 @@ namespace Lumpn.UGUI
 
         public void Apply()
         {
-            // TODO Jonas: do the work
+            var rectTransform = transform as RectTransform;
+            var childCount = rectTransform.childCount;
+            if (childCount < 1) return;
+
+            var anchorStepY = 1 / childCount;
+
+            for (int i = childCount - 1; i >= 0; i--)
+            {
+                var child = rectTransform.GetChild(i) as RectTransform;
+
+                // assuming anchoring to bottom left
+                var j = childCount - i;
+                child.anchorMin = new Vector2(0, anchorStepY * j);
+                child.anchorMax = new Vector2(1, anchorStepY * (j + 1));
+                child.pivot = Vector2.zero;
+            }
         }
     }
 }
