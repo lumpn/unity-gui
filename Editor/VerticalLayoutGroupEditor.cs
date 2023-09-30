83327a11f0b74356a28018e55ee54c163ba34ef5
lumpn <lumpn@users.noreply.github.com>
2023-09-30 22:37:11 +0800

    Support absolute padding and spacing

--- Editor/VerticalLayoutGroupEditor.cs
+++ Editor/VerticalLayoutGroupEditor.cs
@@ -15,21 +15,29 @@ namespace Lumpn.UGUI
                 if (childCount < 1) return;
 
                 var rp = target.relativePadding;
+                var rs = target.relativeSpacing;
                 var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
                 var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
-                var anchorStepY = (1f - rp.top - rp.bottom) / childCount;
+                var anchorStepY = (1f - rp.top - rp.bottom + rs) / childCount;
+                var anchorSpaceY = (1f - rp.top - rp.bottom - (rs * (childCount - 1))) / childCount;
+
+                var tap = target.absolutePadding;
+                var tas = target.absoluteSpacing;
+
+                var sdY = (tap.top + tap.bottom + tas * (childCount - 1)) / childCount;
+                var sd = new Vector2(tap.left + tap.right, sdY);
 
                 for (int i = childCount - 1; i >= 0; i--)
                 {
                     var child = rectTransform.GetChild(i) as RectTransform;
 
-                    // assuming anchoring to bottom left
                     var j = (childCount - 1) - i;
                     child.anchorMin = new Vector2(anchorMin.x, anchorMin.y + anchorStepY * j);
-                    child.anchorMax = new Vector2(anchorMax.x, anchorMin.y + anchorStepY * (j + 1));
-                    child.anchoredPosition = Vector2.zero;
-                    child.sizeDelta = Vector2.zero;
-                    child.pivot = Vector2.zero;
+                    child.anchorMax = new Vector2(anchorMax.x, anchorMin.y + anchorStepY * j + anchorSpaceY);
+
+                    child.anchoredPosition = new Vector2((tap.left - tap.right) / 2, tap.bottom - sdY * (j + 0.5f) + tas * j);
+                    child.sizeDelta = -sd;
+                    child.pivot = new Vector2(0.5f, 0.5f);
 
                     EditorUtility.SetDirty(child);
                 }
