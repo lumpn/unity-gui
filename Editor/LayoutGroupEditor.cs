dae51d5236e25b0eab43c957ff57c5cc62e840ba
lumpn <lumpn@users.noreply.github.com>
2023-09-30 22:54:46 +0800

    Implemented horizontal layout

--- Editor/LayoutGroupEditor.cs
+++ Editor/LayoutGroupEditor.cs
@@ -27,26 +27,56 @@ namespace Lumpn.UGUI
             EditorGUILayout.EndHorizontal();
         }
 
-        public static void ApplyHorizontal(LayoutGroup target)
+        public static void ApplyHorizontal(LayoutGroup layoutGroup)
         {
-            // TODO Jonas: implement
-        }
-
-        public static void ApplyVertical(LayoutGroup target)
-        {
-            var rectTransform = target.transform as RectTransform;
+            var rectTransform = layoutGroup.transform as RectTransform;
             var childCount = rectTransform.childCount;
             if (childCount < 1) return;
 
-            var rp = target.relativePadding;
-            var rs = target.relativeSpacing;
+            var rp = layoutGroup.relativePadding;
+            var rs = layoutGroup.relativeSpacing;
+            var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
+            var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
+            var anchorStepX = (1f - rp.left - rp.right + rs) / childCount;
+            var anchorSpaceX = (1f - rp.left - rp.right - (rs * (childCount - 1))) / childCount;
+
+            var tap = layoutGroup.absolutePadding;
+            var tas = layoutGroup.absoluteSpacing;
+
+            var sdX = (tap.left + tap.right + tas * (childCount - 1)) / childCount;
+            var sd = new Vector2(sdX, tap.top + tap.bottom);
+
+            for (int i = childCount - 1; i >= 0; i--)
+            {
+                var child = rectTransform.GetChild(i) as RectTransform;
+
+                var j = (childCount - 1) - i;
+                child.anchorMin = new Vector2(anchorMin.x + anchorStepX * j, anchorMin.y);
+                child.anchorMax = new Vector2(anchorMin.x + anchorStepX * j + anchorSpaceX, anchorMax.y);
+
+                child.anchoredPosition = new Vector2(tap.left - sdX * (j + 0.5f) + tas * j, (tap.bottom - tap.top) / 2);
+                child.sizeDelta = -sd;
+                child.pivot = new Vector2(0.5f, 0.5f);
+
+                EditorUtility.SetDirty(child);
+            }
+        }
+
+        public static void ApplyVertical(LayoutGroup layoutGroup)
+        {
+            var rectTransform = layoutGroup.transform as RectTransform;
+            var childCount = rectTransform.childCount;
+            if (childCount < 1) return;
+
+            var rp = layoutGroup.relativePadding;
+            var rs = layoutGroup.relativeSpacing;
             var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
             var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
             var anchorStepY = (1f - rp.top - rp.bottom + rs) / childCount;
             var anchorSpaceY = (1f - rp.top - rp.bottom - (rs * (childCount - 1))) / childCount;
 
-            var tap = target.absolutePadding;
-            var tas = target.absoluteSpacing;
+            var tap = layoutGroup.absolutePadding;
+            var tas = layoutGroup.absoluteSpacing;
 
             var sdY = (tap.top + tap.bottom + tas * (childCount - 1)) / childCount;
             var sd = new Vector2(tap.left + tap.right, sdY);
