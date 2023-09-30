544afc2bed659553be1feeeb1ca2841e7fd3cd96
lumpn <lumpn@users.noreply.github.com>
2023-09-30 16:41:35 +0800

    Basic editor

---
+++ Editor/Editor.cs
@@ -0,0 +1,24 @@
+//----------------------------------------
+// MIT License
+// Copyright(c) 2021 Jonas Boetel
+//----------------------------------------
+using UnityEditor;
+using UnityEngine;
+
+namespace Lumpn.GUI
+{
+    public abstract class Editor<T> : Editor where T : Object
+    {
+        public sealed override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.BeginVertical(UnityEngine.GUI.skin.box);
+            OnInspectorGUI((T)target);
+            EditorGUILayout.EndVertical();
+        }
+
+        public abstract void OnInspectorGUI(T target);
+    }
+}
