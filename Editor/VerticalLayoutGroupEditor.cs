d93e0ee83bead114f318a2e0f22d01317a11d205
lumpn <lumpn@users.noreply.github.com>
2023-09-30 17:38:31 +0800

    Working layout

--- Editor/VerticalLayoutGroupEditor.cs
+++ Editor/VerticalLayoutGroupEditor.cs
@@ -10,7 +10,25 @@ namespace Lumpn.UGUI
         {
             if (GUILayout.Button("Apply"))
             {
-                target.Apply();
+                var rectTransform = target.transform as RectTransform;
+                var childCount = rectTransform.childCount;
+                if (childCount < 1) return;
+
+                var anchorStepY = 1f / childCount;
+
+                for (int i = childCount - 1; i >= 0; i--)
+                {
+                    var child = rectTransform.GetChild(i) as RectTransform;
+
+                    // assuming anchoring to bottom left
+                    var j = (childCount - 1) - i;
+                    child.anchorMin = new Vector2(0, anchorStepY * j);
+                    child.anchorMax = new Vector2(1, anchorStepY * (j + 1));
+                    child.pivot = Vector2.zero;
+                    child.sizeDelta = Vector2.zero;
+
+                    EditorUtility.SetDirty(child);
+                }
             }
         }
     }
--- Runtime/VerticalLayoutGroup.cs
+++ Runtime/VerticalLayoutGroup.cs
@@ -12,25 +12,5 @@ namespace Lumpn.UGUI
 
         [SerializeField] private RectOffset padding;
         [SerializeField] private float spacing;
-
-        public void Apply()
-        {
-            var rectTransform = transform as RectTransform;
-            var childCount = rectTransform.childCount;
-            if (childCount < 1) return;
-
-            var anchorStepY = 1 / childCount;
-
-            for (int i = childCount - 1; i >= 0; i--)
-            {
-                var child = rectTransform.GetChild(i) as RectTransform;
-
-                // assuming anchoring to bottom left
-                var j = childCount - i;
-                child.anchorMin = new Vector2(0, anchorStepY * j);
-                child.anchorMax = new Vector2(1, anchorStepY * (j + 1));
-                child.pivot = Vector2.zero;
-            }
-        }
     }
 }
