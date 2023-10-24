7208d3598603a9abc96a3ad02076e21a2080c537
lumpn <lumpn@users.noreply.github.com>
2023-10-25 00:47:13 +0800

    Added content size fitting.
    Moved logic to layout group.

--- Editor/LayoutGroupEditor.cs
+++ Editor/LayoutGroupEditor.cs
@@ -13,86 +13,38 @@ namespace Lumpn.UGUI
         public override void OnInspectorGUI(LayoutGroup target)
         {
             EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("Apply Layout");
 
             if (GUILayout.Button("Horizontal"))
             {
-                ApplyHorizontal(target);
+                target.ApplyHorizontal();
+                EditorUtility.SetDirty(target);
             }
 
             if (GUILayout.Button("Vertical"))
             {
-                ApplyVertical(target);
+                target.ApplyVertical();
+                EditorUtility.SetDirty(target);
             }
 
             EditorGUILayout.EndHorizontal();
-        }
 
-        public static void ApplyHorizontal(LayoutGroup layoutGroup)
-        {
-            var rectTransform = layoutGroup.transform as RectTransform;
-            var childCount = rectTransform.childCount;
-            if (childCount < 1) return;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("Fit Size");
 
-            var rp = layoutGroup.relativePadding;
-            var rs = layoutGroup.relativeSpacing;
-            var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
-            var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
-            var anchorStepX = (1f - rp.left - rp.right + rs) / childCount;
-            var anchorSpaceX = (1f - rp.left - rp.right - (rs * (childCount - 1))) / childCount;
-
-            var tap = layoutGroup.absolutePadding;
-            var tas = layoutGroup.absoluteSpacing;
-
-            var sdX = (tap.left + tap.right + tas * (childCount - 1)) / childCount;
-            var sd = new Vector2(sdX, tap.top + tap.bottom);
-
-            for (int i = childCount - 1; i >= 0; i--)
+            if (GUILayout.Button("Horizontal"))
             {
-                var child = rectTransform.GetChild(i) as RectTransform;
-
-                var j = (childCount - 1) - i;
-                child.anchorMin = new Vector2(anchorMin.x + anchorStepX * j, anchorMin.y);
-                child.anchorMax = new Vector2(anchorMin.x + anchorStepX * j + anchorSpaceX, anchorMax.y);
-
-                child.anchoredPosition = new Vector2(tap.left - sdX * (j + 0.5f) + tas * j, (tap.bottom - tap.top) / 2);
-                child.sizeDelta = -sd;
-
-                EditorUtility.SetDirty(child);
+                target.FitHorizontal();
+                EditorUtility.SetDirty(target);
             }
-        }
 
-        public static void ApplyVertical(LayoutGroup layoutGroup)
-        {
-            var rectTransform = layoutGroup.transform as RectTransform;
-            var childCount = rectTransform.childCount;
-            if (childCount < 1) return;
-
-            var rp = layoutGroup.relativePadding;
-            var rs = layoutGroup.relativeSpacing;
-            var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
-            var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
-            var anchorStepY = (1f - rp.top - rp.bottom + rs) / childCount;
-            var anchorSpaceY = (1f - rp.top - rp.bottom - (rs * (childCount - 1))) / childCount;
-
-            var tap = layoutGroup.absolutePadding;
-            var tas = layoutGroup.absoluteSpacing;
-
-            var sdY = (tap.top + tap.bottom + tas * (childCount - 1)) / childCount;
-            var sd = new Vector2(tap.left + tap.right, sdY);
-
-            for (int i = childCount - 1; i >= 0; i--)
+            if (GUILayout.Button("Vertical"))
             {
-                var child = rectTransform.GetChild(i) as RectTransform;
-
-                var j = (childCount - 1) - i;
-                child.anchorMin = new Vector2(anchorMin.x, anchorMin.y + anchorStepY * j);
-                child.anchorMax = new Vector2(anchorMax.x, anchorMin.y + anchorStepY * j + anchorSpaceY);
-
-                child.anchoredPosition = new Vector2((tap.left - tap.right) / 2, tap.bottom - sdY * (j + 0.5f) + tas * j);
-                child.sizeDelta = -sd;
-
-                EditorUtility.SetDirty(child);
+                target.FitVertical();
+                EditorUtility.SetDirty(target);
             }
+
+            EditorGUILayout.EndHorizontal();
         }
     }
 }
--- Runtime/LayoutGroup.cs
+++ Runtime/LayoutGroup.cs
@@ -9,26 +9,107 @@ namespace Lumpn.UGUI
     public sealed class LayoutGroup : MonoBehaviour
     {
         [System.Serializable]
-        public struct RectOffset
+        private struct RectOffset
         {
-            [SerializeField] float _left, _right, _top, _bottom;
-
-            public float left => _left;
-            public float right => _right;
-            public float top => _top;
-            public float bottom => _bottom;
+            [SerializeField] public float left, right, top, bottom;
         }
 
-        [SerializeField] private RectOffset _relativePadding;
-        [SerializeField] private float _relativeSpacing;
+        [SerializeField] private RectOffset relativePadding;
+        [SerializeField] private float relativeSpacing;
 
-        [SerializeField] private RectOffset _absolutePadding;
-        [SerializeField] private float _absoluteSpacing;
+        [SerializeField] private RectOffset absolutePadding;
+        [SerializeField] private float absoluteSpacing;
 
-        public RectOffset relativePadding => _relativePadding;
-        public float relativeSpacing => _relativeSpacing;
+        [SerializeField] private Vector2 childSize;
 
-        public RectOffset absolutePadding => _absolutePadding;
-        public float absoluteSpacing => _absoluteSpacing;
+        public void FitHorizontal()
+        {
+            var rectTransform = (RectTransform)transform;
+            var childCount = rectTransform.childCount;
+
+            var size = childCount * childSize.x;
+            size += absolutePadding.left;
+            size += absolutePadding.right;
+            size += absoluteSpacing * Mathf.Max(childCount - 1, 0);
+
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+        }
+
+        public void FitVertical()
+        {
+            var rectTransform = (RectTransform)transform;
+            var childCount = rectTransform.childCount;
+
+            var size = childCount * childSize.y;
+            size += absolutePadding.top;
+            size += absolutePadding.bottom;
+            size += absoluteSpacing * Mathf.Max(childCount - 1, 0);
+
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
+        }
+
+        public void ApplyHorizontal()
+        {
+            var rectTransform = (RectTransform)transform;
+            var childCount = rectTransform.childCount;
+            if (childCount < 1) return;
+
+            var rp = relativePadding;
+            var rs = relativeSpacing;
+            var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
+            var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
+            var anchorStepX = (1f - rp.left - rp.right + rs) / childCount;
+            var anchorSpaceX = (1f - rp.left - rp.right - (rs * (childCount - 1))) / childCount;
+
+            var tap = absolutePadding;
+            var tas = absoluteSpacing;
+
+            var sdX = (tap.left + tap.right + tas * (childCount - 1)) / childCount;
+            var sd = new Vector2(sdX, tap.top + tap.bottom);
+
+            for (int i = childCount - 1; i >= 0; i--)
+            {
+                var child = rectTransform.GetChild(i) as RectTransform;
+
+                var j = (childCount - 1) - i;
+                child.anchorMin = new Vector2(anchorMin.x + anchorStepX * j, anchorMin.y);
+                child.anchorMax = new Vector2(anchorMin.x + anchorStepX * j + anchorSpaceX, anchorMax.y);
+
+                child.anchoredPosition = new Vector2(tap.left - sdX * (j + 0.5f) + tas * j, (tap.bottom - tap.top) / 2);
+                child.sizeDelta = -sd;
+            }
+        }
+
+        public void ApplyVertical()
+        {
+            var rectTransform = (RectTransform)transform;
+            var childCount = rectTransform.childCount;
+            if (childCount < 1) return;
+
+            var rp = relativePadding;
+            var rs = relativeSpacing;
+            var anchorMin = Vector2.zero + new Vector2(rp.left, rp.bottom);
+            var anchorMax = Vector2.one - new Vector2(rp.right, rp.top);
+            var anchorStepY = (1f - rp.top - rp.bottom + rs) / childCount;
+            var anchorSpaceY = (1f - rp.top - rp.bottom - (rs * (childCount - 1))) / childCount;
+
+            var tap = absolutePadding;
+            var tas = absoluteSpacing;
+
+            var sdY = (tap.top + tap.bottom + tas * (childCount - 1)) / childCount;
+            var sd = new Vector2(tap.left + tap.right, sdY);
+
+            for (int i = childCount - 1; i >= 0; i--)
+            {
+                var child = rectTransform.GetChild(i) as RectTransform;
+
+                var j = (childCount - 1) - i;
+                child.anchorMin = new Vector2(anchorMin.x, anchorMin.y + anchorStepY * j);
+                child.anchorMax = new Vector2(anchorMax.x, anchorMin.y + anchorStepY * j + anchorSpaceY);
+
+                child.anchoredPosition = new Vector2((tap.left - tap.right) / 2, tap.bottom - sdY * (j + 0.5f) + tas * j);
+                child.sizeDelta = -sd;
+            }
+        }
     }
 }
